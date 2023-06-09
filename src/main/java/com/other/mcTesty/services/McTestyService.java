8710e29238b5de5283ff296121673d122943f2d0
Ayla Winters <ayla.entrekin@gmail.com>
2023-06-09 14:53:56 -0500

    removed dependencies to add dep-layer

--- src/main/java/com/other/mcTesty/services/McTestyService.java
+++ src/main/java/com/other/mcTesty/services/McTestyService.java
@@ -1,40 +1,28 @@
 package com.other.mcTesty.services;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
-import com.other.mcTesty.entities.Product;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import com.dep.layer.gcp.FireStoreConfig;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class McTestyService {
 
-    private Firestore db;
+    private final Firestore db;
 
-    @Autowired
-    public McTestyService(Firestore db) { this.db = db; }
+    public McTestyService() { this.db = FireStoreConfig.getInstance(); }
+    
+   
 
-    public ResponseEntity<Object> addDocument(Product product){
-        try {
-            DocumentReference docRef = db.collection("products").document(product.getName());
-            ApiFuture<WriteResult> result = docRef.set(product);
-            result.get();
-        } catch (Exception e){
-            e.printStackTrace();
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>("Success! " + product.getName() + " was added!", HttpStatus.OK);
-    }
     public ResponseEntity<Object> addJsonObject(JSONObject jsonObject){
         Map<String, Object> data = new HashMap<>();
         try {
