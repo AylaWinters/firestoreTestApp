02bcc950907d2929d1cc06d5818c605a4133d234
Entrekin <entrekina@dnb.com>
2023-07-31 15:10:11 -0500

    updated file names and README

--- src/main/java/com/other/mcTesty/services/McTestyService.java
+++ src/main/java/com/other/mcTesty/services/McTestyService.java
@@ -6,6 +6,7 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,15 @@ import java.util.Map;
 public class McTestyService {
 
     private final Firestore db;
-	
+
+    @Autowired
     public McTestyService(Firestore db) { this.db = db; }
 
     public ResponseEntity<Object> addJsonObject(JSONObject jsonObject){
         Map<String, Object> data = new HashMap<>();
         try {
-        	if(jsonObject.has("name") && jsonObject.getString("name") != null) {
-        		DocumentReference docRef = db.collection("products").document(jsonObject.getString("name"));
+        	if(jsonObject.has("Table_Name") && jsonObject.getString("Table_Name") != null) {
+        		DocumentReference docRef = db.collection("Collection_Name").document(jsonObject.getString("Table_Name"));
         		for (String key : jsonObject.keySet()) {
         			Object value = jsonObject.get(key);
         			data.put(key, value);
@@ -33,14 +35,14 @@ public class McTestyService {
         		ApiFuture<WriteResult> result = docRef.set(data);
         		result.get();
            	} else {
-           		return new ResponseEntity<>("Key: 'name' was not provided",
+           		return new ResponseEntity<>("Key: 'Table_Name' was not provided",
            				HttpStatus.BAD_REQUEST);
            	}
         } catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>("Success! " + jsonObject.getString("name")
+        return new ResponseEntity<>("Success! " + jsonObject.getString("Table_Name")
                 + " was added!", HttpStatus.OK);
     }
 
